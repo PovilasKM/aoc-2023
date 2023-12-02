bb3ce508098026a7c5de18444fa5d30623251c6e
Kazimieras Povilas Maceika (fxl115) <kazimieras-povilas.maceika@teliacompany.com>
2023-12-02 12:13:36 +0200

    DAY2: I guess it works?

--- day2/main1.py
+++ day2/main1.py
@@ -1,3 +1,20 @@
+import re
 
-for line in open('data.txt', "r"):
-    print(line)
+red_limit = 12
+green_limit = 13
+blue_limit = 14
+total = 0
+for line in [x for x in open('data.txt', "r").read().split("\n") if x]:
+    game_id = int(line.split(':')[0].split('Game ')[1])
+    possible = True
+    for part in line.split(':')[1].split(';'):
+        colors = {'red': 0, 'green': 0, 'blue': 0}
+        for num_col in part.split(','):
+            groups = re.search('([0-9]+)\\s(red|blue|green)', num_col)
+            colors[groups[2]] = colors[groups[2]] + int(groups[1])
+            if colors['red'] > red_limit or colors['blue'] > blue_limit or colors['green'] > green_limit:
+                possible = False
+                break
+    if possible:
+        total += game_id
+print(total)
--- day2/main2.py
+++ day2/main2.py
@@ -1,3 +1,19 @@
+import re
 
-for line in open('data.txt', "r"):
-    print(line)
+total = 0
+for line in [x for x in open('data.txt', "r").read().split("\n") if x]:
+    game_id = int(line.split(':')[0].split('Game ')[1])
+    colors_max = {'red': 0, 'green': 0, 'blue': 0}
+    for part in line.split(':')[1].split(';'):
+        colors = {'red': 0, 'green': 0, 'blue': 0}
+        for num_col in part.split(','):
+            groups = re.search('([0-9]+)\\s(red|blue|green)', num_col)
+            colors[groups[2]] = colors[groups[2]] + int(groups[1])
+        if colors['red'] > colors_max['red']:
+            colors_max['red'] = colors['red']
+        if colors['green'] > colors_max['green']:
+            colors_max['green'] = colors['green']
+        if colors['blue'] > colors_max['blue']:
+            colors_max['blue'] = colors['blue']
+    total += colors_max['red'] * colors_max['blue'] * colors_max['green']
+print(total)
