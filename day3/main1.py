c36890959b21fb8852de0dc1b594a4cafe4e5e41
Kazimieras Povilas Maceika (fxl115) <kazimieras-povilas.maceika@teliacompany.com>
2023-12-03 19:53:14 +0200

    DAY3: turning set into a dict mid execution is not a great idea

--- day3/main1.py
+++ day3/main1.py
@@ -1,3 +1,34 @@
+def do_surroundings_contain_symbol(lines, x, y):
+    symbols = [lines[y - 1][x - 1], lines[y - 1][x], lines[y - 1][x + 1],
+               lines[y][x - 1], lines[y][x + 1],
+               lines[y + 1][x - 1], lines[y + 1][x], lines[y + 1][x + 1]]
+    non_num_symbols = list(filter(lambda a: (not a.isdigit()) and (a != '.'), symbols))
+    return len(non_num_symbols) > 0
 
-for line in open('data.txt', "r"):
-    print(line)
+
+total = 0
+lines = [x for x in open('data.txt', "r").read().split("\n") if x]
+
+# append the array
+for x in range(len(lines)):
+    lines[x] = '.' + lines[x] + '.'
+lines.insert(0, '.' * len(lines[0]))
+lines.append('.' * len(lines[0]))
+
+for y, line in enumerate(lines[1:len(lines) - 1]):
+    num = ''
+    is_part_number = False
+    for x in range(1, len(line)):
+        symbol = line[x]
+        if symbol.isdigit():
+            num += symbol
+            if do_surroundings_contain_symbol(lines, x, y+1):
+                is_part_number = True
+        else:  # not a number
+            if num.isdigit() and is_part_number:  # if previous symbol was a number and the whole thing touched a symbol
+                # print(num)
+                total += int(num)
+            num = ''
+            is_part_number = False
+
+print(total)
--- day3/main2.py
+++ day3/main2.py
@@ -1,3 +1,38 @@
+def get_surrounding_gears(lines, x, y):
+    surrounding_points = [(x - 1, y - 1), (x, y - 1), (x + 1, y - 1),
+                          (x - 1, y), (x + 1, y),
+                          (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)]
+    return list(filter(lambda a: lines[a[1]][a[0]] == '*', surrounding_points))
 
-for line in open('data.txt', "r"):
-    print(line)
+
+total = 0
+lines = [x for x in open('data.txt', "r").read().split("\n") if x]
+
+# append the array
+for x in range(len(lines)):
+    lines[x] = '.' + lines[x] + '.'
+lines.insert(0, '.' * len(lines[0]))
+lines.append('.' * len(lines[0]))
+
+gears = {}
+for y, line in enumerate(lines[1:len(lines) - 1]):
+    num = ''
+    temp_gears = set()
+    for x in range(1, len(line)):
+        symbol = line[x]
+        if symbol.isdigit():
+            num += symbol
+            temp_gears.update(get_surrounding_gears(lines, x, y + 1))
+        elif num.isdigit():  # not a number
+            for temp_gear in temp_gears:
+                if temp_gear not in gears.keys():
+                    gears[temp_gear] = []
+                gears[temp_gear].append(int(num))
+            num = ''
+            temp_gears = set()
+
+for value in gears.values():
+    if len(value) == 2:
+        total += value[0] * value[1]
+
+print(total)
