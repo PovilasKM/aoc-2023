f74380423157862d7c730908ddd71ad881939dc6
Kazimieras Povilas Maceika (fxl115) <kazimieras-povilas.maceika@teliacompany.com>
2023-12-04 10:47:22 +0200

    DAY4: this was easier than day1 for some reason...

--- day4/main1.py
+++ day4/main1.py
@@ -1,3 +1,9 @@
-
-for line in open('data.txt', "r"):
-    print(line)
+total = 0
+for line in [x for x in open('data.txt', "r").read().split("\n") if x]:
+    number_lists = line.split(':')[1]
+    winning_numbers = [int(x) for x in number_lists.split('|')[0].strip().split(' ') if x]
+    my_numbers = [int(x) for x in number_lists.split('|')[1].strip().split(' ') if x]
+    winning_count = len(list(filter(lambda x: x in winning_numbers, my_numbers))) - 1
+    if winning_count >= 0:
+        total += pow(2, winning_count)
+print(total)
--- day4/main2.py
+++ day4/main2.py
@@ -1,3 +1,16 @@
+cards = {}  # card1: [winning_count, card_count]
+for i, line in enumerate([x for x in open('data.txt', "r").read().split("\n") if x]):
+    number_lists = line.split(':')[1]
+    winning_numbers = [int(x) for x in number_lists.split('|')[0].strip().split(' ') if x]
+    my_numbers = [int(x) for x in number_lists.split('|')[1].strip().split(' ') if x]
+    winning_count = len(list(filter(lambda x: x in winning_numbers, my_numbers)))
+    cards[i + 1] = [winning_count, 1]
 
-for line in open('data.txt', "r"):
-    print(line)
+for key, current_card in cards.items():
+    winning_count = current_card[0]
+    current_card_count = current_card[1]
+    for i in range(1, winning_count + 1):
+        if key + i in cards.keys():
+            cards[key + i][1] += current_card_count
+
+print(sum([x[1] for x in cards.values()]))
