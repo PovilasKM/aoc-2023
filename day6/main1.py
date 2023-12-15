d91561ef127b7e3f135ac23f5cbf4b84251b5c10
Kazimieras Povilas Maceika (fxl115) <kazimieras-povilas.maceika@teliacompany.com>
2023-12-15 13:30:16 +0200

    DAY6: that was way easier than day 5...

--- day6/main1.py
+++ day6/main1.py
@@ -1,3 +1,17 @@
+from functools import reduce
 
-for line in open('data.txt', "r"):
-    print(line)
+lines = [x for x in open('data.txt', "r").read().split("\n") if x]
+times = [int(x) for x in lines[0].split(':')[1].split(' ') if x.isdigit()]
+distances = [int(x) for x in lines[1].split(':')[1].split(' ') if x.isdigit()]
+races = list(zip(times, distances))
+ways_to_bear_the_record = [0 for x in times]
+
+for i, race in enumerate(races):
+    for time_waited in range(1, race[0]):
+        speed = time_waited
+        remaining_time = race[0] - time_waited
+        target_distance = race[1]
+        if speed * remaining_time > target_distance:
+            ways_to_bear_the_record[i] += 1
+
+print(reduce(lambda x, y: x*y, ways_to_bear_the_record))
--- day6/main2.py
+++ day6/main2.py
@@ -1,3 +1,14 @@
+from functools import reduce
 
-for line in open('data.txt', "r"):
-    print(line)
+lines = [x for x in open('data.txt', "r").read().split("\n") if x]
+time = int(lines[0].split(':')[1].replace(' ', ''))
+distance = int(lines[1].split(':')[1].replace(' ', ''))
+ways_to_bear_the_record = 0
+
+for time_waited in range(1, time):
+    speed = time_waited
+    remaining_time = time - time_waited
+    if speed * remaining_time > distance:
+        ways_to_bear_the_record += 1
+
+print(ways_to_bear_the_record)
