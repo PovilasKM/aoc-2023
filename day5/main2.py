4956f2dbc606bcce7e77ce6efe5fb43a95c38053
Kazimieras Povilas Maceika (fxl115) <kazimieras-povilas.maceika@teliacompany.com>
2023-12-15 13:02:03 +0200

    DAY5: covid's over, muh brain works again

--- day5/main2.py
+++ day5/main2.py
@@ -12,7 +12,7 @@ for seed_interval in seed_intervals:
     intervals = [seed_interval]
     i = 3
     while i < len(lines):
-        print('intervals: ', intervals)
+        # print('intervals: ', intervals)
         nums = [int(x) for x in lines[i].split(' ')]
         distance = nums[2] - 1
         source_start = nums[1]
@@ -21,17 +21,14 @@ for seed_interval in seed_intervals:
         destination_end = destination_start + distance
         change = destination_start - source_start
 
-        print('applying: ' + lines[i] + ': ', f'destination: ({destination_start}, {destination_end}), source: ({source_start}, {source_end})')
         for interval in [x for x in intervals if x[2] == 0]:  # only use intervals that are marked as unused (0)
             #     source(map/rule) covers the interval
             if interval[0] >= source_start and interval[1] <= source_end:
-                print('map covers')
                 intervals.append([interval[0] + change, interval[1] + change, 1])
                 interval[2] = -1  # mark interval as used and up for removal
 
             #     source(map/rule) is covered by the interval
             elif interval[0] < source_start and interval[1] > source_end:
-                print('map is covered')
                 intervals.append([interval[0], source_start - 1, 0])
                 intervals.append([source_start + change, source_end + change, 1])
                 intervals.append([source_end + 1, interval[1], 0])
@@ -39,28 +36,34 @@ for seed_interval in seed_intervals:
 
             #    sources(map/rule) covers the interval from the left
             elif (source_start < interval[0] <= source_end) and interval[1] > source_end:
-                print('map covers beginning')
                 intervals.append([interval[0] + change, source_end + change, 1])
                 intervals.append([source_end + 1, interval[1], 0])
                 interval[2] = -1  # mark interval as used and up for removal
 
             #    source(map/rule) covers the interval from the right
             elif (interval[0] <= source_start < interval[1]) and interval[1] < source_end:
-                print('map covers end')
                 intervals.append([interval[0], source_start - 1, 0])
                 intervals.append([source_start + change, interval[1] + change, 1])
                 interval[2] = -1  # mark interval as used and up for removal
-            print('intervals after mapping: ', end='')
-            print(intervals)
+
+            #     source(map/rule) is covered by the interval, end matches
+            elif interval[0] < source_start and interval[1] == source_end:
+                intervals.append([interval[0], source_start - 1, 0])
+                intervals.append([source_start + change, source_end + change, 1])
+                interval[2] = -1  # mark interval as used and up for removal
+
+            # source(map/rule) is covered by the interval, start matches
+            elif interval[0] == source_start and interval[1] > source_end:
+                intervals.append([source_start + change, source_end + change, 1])
+                intervals.append([source_end + 1, interval[1], 0])
+                interval[2] = -1  # mark interval as used and up for removal
+
         i += 1
-        if not lines[i]:
-            print('\n')
+        if not lines[i]:  # empty line -> next rules/maps block is starting, perform a cleanup
             i += 2
             intervals = list(filter(lambda x: x[2] != -1, intervals))  # remove intervals that have been used
             for interval in intervals:
                 interval[2] = 0
 
-    print('@@', intervals, min([x[0] for x in intervals]))
     minimums.append(min([x[0] for x in intervals]))
-print(minimums)
 print(min(minimums))
