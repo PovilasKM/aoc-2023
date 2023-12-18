579ebed229dcdad36f09da59e4db23e3ac812694
Kazimieras Povilas Maceika (fxl115) <kazimieras-povilas.maceika@teliacompany.com>
2023-12-18 16:21:10 +0200

    DAY9: that was surprisingly simple

--- day9/main1.py
+++ day9/main1.py
@@ -1,3 +1,10 @@
-
-for line in open('data.txt', "r"):
-    print(line)
+lines = [x for x in open('data.txt', "r").read().split("\n") if x]
+total = 0
+for line in lines:
+    numbers = [int(x) for x in line.split(' ')]
+    next_num = 0
+    while set(numbers) != {0}:
+        next_num += numbers[-1]
+        numbers = [current - previous for previous, current in zip(numbers, numbers[1:])]
+    total += next_num
+print(total)
--- day9/main2.py
+++ day9/main2.py
@@ -1,3 +1,15 @@
+def get_diffs(numbers):
+    new_numbers = [current - previous for previous, current in zip(numbers, numbers[1:])]
+    if set(numbers) == {0}:
+        return numbers[0] - new_numbers[0]
+    else:
+        return numbers[0] - get_diffs(new_numbers)
 
-for line in open('data.txt', "r"):
-    print(line)
+
+lines = [x for x in open('data.txt', "r").read().split("\n") if x]
+total = 0
+
+for line in lines:
+    numbers = [int(x) for x in line.split(' ')]
+    total += get_diffs(numbers)
+print(total)
