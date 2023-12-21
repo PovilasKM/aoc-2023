86a90ab93101ec3b0bb6374d5a7046b4a5c090c0
Kazimieras Povilas Maceika (fxl115) <kazimieras-povilas.maceika@teliacompany.com>
2023-12-21 12:14:32 +0200

    DAY11: part2 was quick

--- day11/main2.py
+++ day11/main2.py
@@ -1,3 +1,29 @@
+lines = [x for x in open('data.txt', "r").read().split("\n") if x]
 
-for line in open('data.txt', "r"):
-    print(line)
+empty_rows = []
+for i in range(0, len(lines)):
+    if set([x for x in lines[i][:]]) == {'.'}:
+        empty_rows.append(i)
+
+empty_columns = []
+for i in range(0, len(lines[0])):
+    if set([x[i] for x in lines]) == {'.'}:
+        empty_columns.append(i)
+
+galaxies = []
+for y in range(0, len(lines)):
+    for x in range(0, len(lines[0])):
+        if lines[y][x] == '#':
+            galaxies.append((x, y))
+
+total = 0
+for i, start_galaxy in enumerate(galaxies[:-1]):
+    for destination_galaxy in galaxies[i + 1:]:
+        x_base = abs(start_galaxy[0] - destination_galaxy[0])
+        y_base = abs(start_galaxy[1] - destination_galaxy[1])
+        base_distance = max(x_base + y_base, 1)
+        x_empty_galaxies = len([x for x in empty_columns if min(start_galaxy[0], destination_galaxy[0]) < x < max(start_galaxy[0], destination_galaxy[0])])
+        y_empty_galaxies = len([y for y in empty_rows if min(start_galaxy[1], destination_galaxy[1]) < y < max(start_galaxy[1], destination_galaxy[1])])
+        total += base_distance + (x_empty_galaxies * 999_999) + (y_empty_galaxies * 999_999)
+
+print(total)
---
+++ day9/main2_1.py
@@ -0,0 +1,10 @@
+lines = [x for x in open('data.txt', "r").read().split("\n") if x]
+total = 0
+for line in lines:
+    numbers = [int(x) for x in line.split(' ')][::-1]
+    next_num = 0
+    while set(numbers) != {0}:
+        next_num += numbers[-1]
+        numbers = [current - previous for previous, current in zip(numbers, numbers[1:])]
+    total += next_num
+print(total)
